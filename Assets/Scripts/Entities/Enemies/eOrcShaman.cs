cc819449f212a69ba4ff55af81265664d1e6ecce
Damian Mendez <kiliranshe@hotmail.com>
2022-05-23 19:19:41 +0200

    OrcShaman: si se aleja una distancia maxima, se queda quieto esperando a disparar.

--- Assets/Scripts/Entities/Enemies/eOrcShaman.cs
+++ Assets/Scripts/Entities/Enemies/eOrcShaman.cs
@@ -33,6 +33,12 @@ public class eOrcShaman : Enemy
 
     protected override void EnemyBehaviour()
     {
+        MovementEnemyBehaviour();
+      
+    }
+
+    protected override void MovementEnemyBehaviour()
+    {
         if (Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position) >= minRange)
         {
             FollowPlayer();
@@ -43,44 +49,17 @@ public class eOrcShaman : Enemy
         {
             isMoving = false;
             animator.SetBool("isMoving", isMoving);
-            //goRespawn();
+            ShootBehaviour();
         }
+    }
 
-        //if (passingTime < inmuneTime)
-        //{
-        //    passingTime += Time.deltaTime;
-        //    enemyInmune = true;
-        //}
-        //else
-        //{
-        //    enemyInmune = false;
-        //}
-
-        // shooting enemy
+    private void ShootBehaviour()
+    {
         if (timeBtwShots <= 0)
         {
-            //Raycast si hay vision
-            Vector3 directionRay = (GameManager.instance.player.transform.position - transform.position).normalized;
-            RaycastHit2D hitVision = Physics2D.Raycast(transform.position + (directionRay * 1), directionRay, attackRange, whatIsSolid);
-
-            //debug
-            Debug.DrawRay(transform.position + (directionRay * 1), directionRay * attackRange, Color.green, 0.1f);
-
-            if (hitVision.collider != null)
-            {
-                if (hitVision.collider.CompareTag("Player"))
-                {
-                    timeBtwShots = startTimeBtwShots;
-                    // control para girar el punto de disparo hacia el jugador
-                    Vector3 difference =  (transform.position - GameManager.instance.player.transform.position);
-                    float rotZ = (Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg);
-                    shotPoint.rotation = Quaternion.Euler(0f, 0f, rotZ + 90);
-
-                    GameObject shot = Instantiate(enemyShot, shotPoint.position,shotPoint.rotation);
-                    shot.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(0f, webForce, 0f));
-                    
-                }
-            }
+            RaycastHit2D hitVision = RaycastVisionToPlayer();
+            //Debug.DrawRay(transform.position + (directionRay * 1), directionRay * attackRange, Color.green, 0.1f);
+            CheckCollisionAndShoot(hitVision);
         }
         else
         {
@@ -88,9 +67,41 @@ public class eOrcShaman : Enemy
         }
     }
 
+    private RaycastHit2D RaycastVisionToPlayer()
+    {
+        Vector3 directionRay = (GameManager.instance.player.transform.position - transform.position).normalized;
+        return Physics2D.Raycast(transform.position + (directionRay * 1), directionRay, attackRange, whatIsSolid);
+
+    }
+
+    private void CheckCollisionAndShoot(RaycastHit2D hitVision)
+    {
+        if (hitVision.collider != null)
+        {
+            if (hitVision.collider.CompareTag("Player"))
+            {
+                timeBtwShots = startTimeBtwShots;
+                shotPoint.rotation = CalculateRotationToTarget();
+                InstantiateShoot();
+            }
+        }
+    }
+
+    private Quaternion CalculateRotationToTarget()
+    {
+        Vector3 difference = (transform.position - GameManager.instance.player.transform.position);
+        float rotZ = (Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg);
+       return Quaternion.Euler(0f, 0f, rotZ + 90);
+    }
+
+    private void InstantiateShoot()
+    {
+        GameObject shot = Instantiate(enemyShot, shotPoint.position, shotPoint.rotation);
+        shot.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(0f, webForce, 0f));
+    }
+
     void OnDrawGizmos()
     {
-        ////debug dibujar las fintas y direccion del muñeco
         //Handles.color = Color.green;
         //Handles.DrawLine(transform.position, playerPosition);
     }
